189c0f0b69eae1b287b865d8ff76b6a4dcaf6070
JonGudenzi <jongudenzi@gmail.com>
2025-11-22 12:36:32 -0500

    wiring js

---
+++ script.js
@@ -0,0 +1,7 @@
+const favForm = document.getElementById("favForm");
+const favInput = document.getElementById("favInput");
+const errorMsg = document.getElementById("errorMsg");
+const favList = document.getElementById("favList");
+
+let favorites = [];
+
