eaf95c5b9a82ee6f63dc94f394b9ef8c96efe151
JonGudenzi <jongudenzi@gmail.com>
2025-11-22 12:50:22 -0500

    event listener for submit of new li. rendering li and delete button

--- script.js
+++ script.js
@@ -5,3 +5,42 @@ const favList = document.getElementById("favList");
 
 let favorites = [];
 
+favForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const userInput = favInput.value.trim();
+    if(userInput === "") {
+        errorMsg.textContent = "Please enter a favorite item";
+        favInput.focus();
+        return;
+    }
+    errorMsg.textContent = "";
+    favInput.value = "";
+    favInput.focus();
+    favorites.push(userInput);
+    saveFavorites();
+    render();
+}
+
+function render() {
+    favList.innerHTML = "";
+    favorites.forEach(function(item, index) {
+        const li = document.createElement("li");
+        li.textContent = item;
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "X";
+        deleteBtn.classList.add("delete-btn");
+        li.appendChild(deleteBtn);
+        deleteBtn.addEventListener("click", function() {
+            favorites.splice(index, 1);
+            render();
+        })
+        favList.appendChild(li);
+    })
+}
+
+function saveFavorites() {
+    const data = JSON.stringify(favorites);
+    localStorage.setItem("favorites", data);
+}
